4b7881990fa2d8fdec990dfc2f4370ced35dcc57
DhruvKauts2 <kautsdhruv15@gmail.com>
2020-07-30 22:45:37 +0530

    Mobile app

---
+++ Mobile/pec_connect/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:CWCFlutter/food_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/food_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<FoodBloc>(
+      create: (context) => FoodBloc(),
+      child: MaterialApp(
+        title: 'Sqflite Tutorial',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: FoodList(),
+      ),
+    );
+  }
+}
+
+
